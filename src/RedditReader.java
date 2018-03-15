a5f61124f94edc5ef43d489ee72283556c833e9f
GautamNaidu <36931663+GautamNaidu@users.noreply.github.com>
2018-03-15 11:27:20 -0400

    Final Upload

--- src/RedditReader.java
+++ src/RedditReader.java
@@ -16,13 +16,11 @@ public class RedditReader {
 		File fileOne = new File(inputFile); //File Object
 		FileReader FR = null; //File Reader Object
 		BufferedReader BR = null; //Buffered Reader Object
-		ArrayList<String> commentsArray = new ArrayList<String>(); //Array of all comments from .txt file.
-		ArrayList<String> wordSpecificArray = new ArrayList<String>(); //Array of all comments that have the desired word in them.
-		Scanner input = new Scanner(System.in); //Scanner Object 
-		double percentage;
-		String[] words;
-		Map<String, Integer> wordMap = new HashMap<String, Integer>();
-
+		String[] words; //A temporary array that stores words from the line being analyzed.   
+		Map<String, Integer> wordMap = new HashMap<String, Integer>(); //Hash Table utilized to keep track of various words mentioned and the number of times they're mentioned.
+		String k; //A temporary value that contains a key value from the Hash Table.
+		Integer v;//A temporary value that contains the list value for the corresponding key value (variable k).
+		
 		//Try Catch Utilized for File Reading and Buffered Reading...
 		try {
 			
@@ -31,48 +29,41 @@ public class RedditReader {
 			
 			while (BR.ready()) { //BR.ready checks to see if there is another line that can be read below. Returns Boolean Type.
 				String TempVar = BR.readLine(); //Takes the line and saves it to temporary variable "TempVar".
-				words = TempVar.split(" ");
-				for (int i=0; i<words.length; i++) 
+				TempVar = TempVar.toLowerCase(); //Turns the line saved in the TempVar to all lower case.
+				words = TempVar.split(" "); //Breaks down the line into an array of words. 
+				for (int i=0; i<words.length; i++) //For loop used to analyze each individual word in the array.
 				{
-					String w = words[i];
-					w = w.replaceAll("[-+.^:,?]","");					
-					if (wordMap.get(w)==null) 
+					String w = words[i]; //Another temporary variable that saves the individual word out of the array.
+					w = w.replaceAll("[-+.^:,?]",""); //This replaces any of the special characters listed with nothing or in other words "deletes them".					
+					if (wordMap.get(w)==null) //This if statement processes the word and determines whether the word already exists in the Hash Table or if a new key/list pair needs to be created.
 					{
-						wordMap.put(w, 1);
+						wordMap.put(w, 1); //If the word doesn't exist in the Hash Table, this creates a new entry.
 					}
 					else
 					{
-						wordMap.put(w, wordMap.get(w)+1);						
+						wordMap.put(w, wordMap.get(w)+1); //If the word exists already, add one to the pre-existing list value.					
 					}
 				}
-				 
-				commentsArray.add(TempVar); //Adds the line to the array list.
-
-				TempVar = TempVar.toLowerCase(); //Turns the line saved in the TempVar to all lower case.
-
 			}
-			percentage = ((double)wordSpecificArray.size()/(double)commentsArray.size())*100;
-			percentage = Math.round(percentage * 100.0)/100.0;
 
 			BR.close(); //Closes out of the file.
 			
-			String k;
-			Integer v;
-			Set<String> keyList = wordMap.keySet();
 			
-			while (keyList.isEmpty() != true)
+			Set<String> keyList = wordMap.keySet(); //This variable is now given the list of keys from the Hash Table.
+			
+			while (keyList.isEmpty() != true) //While loop that checks whether the list of keys contains a value.
 			{
-				v = new Integer(0);
+				v = new Integer(0); //Wrapper Class, set to zero.
 				k=null;
-				for(String key: keyList){
-					if (v < wordMap.get(key))
+				for(String key: keyList){ //For loop runs through the list of keys one by one.
+					if (v < wordMap.get(key)) // If statement that compares the previous key list value to the current value of the for loop. This determines which value is the greatest in the key list.
 					{
-						k = key;
-						v = wordMap.get(key);
+						k = key; //The key entry that is being processed by the loop.
+						v = wordMap.get(key); //The corresponding value to the key.
 					}
 				}
-				System.out.println( k + " - " + v);
-				keyList.remove(k);
+				System.out.println( k + " - " + v); //After comparing all the key list values. The largest values are printed here.
+				keyList.remove(k); //This removes the greatest key entry out of the list of keys.
 			}
 			
 
--- src/RedditTester.java
+++ src/RedditTester.java
@@ -1,12 +1,6 @@
 
 public class RedditTester {
-
-	public static void main(String[] args) {
-		
-		RedditReader russiaCounter = new RedditReader("RedditComments.txt");
-				
-			
-			
+	public static void main(String[] args) {	
+		RedditReader wordSorter = new RedditReader("RedditComments.txt");	//This is the file you run. This calls on RedditReader Method.
 	}
-
 }
