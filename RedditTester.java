7b516394452beed4f8d477d251dd501a2fbe33fc
GautamNaidu <36931663+GautamNaidu@users.noreply.github.com>
2018-03-18 19:27:39 -0400

    Delete RedditTester.java

--- RedditTester.java
+++
@@ -1,6 +0,0 @@
-
-public class RedditTester {
-	public static void main(String[] args) {	
-		RedditReader wordSorter = new RedditReader("RedditComments.txt");	//This is the file you run. This calls on RedditReader Method.
-	}
-}
